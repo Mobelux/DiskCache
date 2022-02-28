07c65d6d8c282bd95074c22f267050b6172a0278
Jeremy Greenwood <jeremy@mobelux.com>
2022-02-28 14:42:39 -0500

    add synchronous variants (#4)

--- Sources/DiskCache/StorageType.swift
+++ Sources/DiskCache/StorageType.swift
@@ -11,11 +11,11 @@ public typealias AppGroupID = String
 
 public enum StorageType {
     // stores data in user's `caches` directory, which is volatile
-    case temporary(SubDirectory?)
+    case temporary(_ subDirectory: SubDirectory? = nil)
     // stores data in user's `directory` directory
-    case permanent(SubDirectory?)
+    case permanent(_ subDirectory: SubDirectory? = nil)
     // stores data in shared container, which is suitable to share data between app, extenstions, etc
-    case shared(AppGroupID, SubDirectory?)
+    case shared(_ appGroupID: AppGroupID, subDirectory: SubDirectory? = nil)
 
     var subDirectory: String? {
         switch self {

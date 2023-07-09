84e602c506e183a730bc6734e6ed394470c6d99f
Matt Gacy <matt@mgacy.com>
2023-07-09 12:01:46 -0700

    Add documentation

--- Sources/DiskCache/Cache.swift
+++ Sources/DiskCache/Cache.swift
@@ -7,16 +7,49 @@
 
 import Foundation
 
+/// A class of types providing interfaces for caching and retrieving data to/from disk.
 public protocol Cache {
+
+    /// Synchronously writes `data` to disk.
+    /// - Parameters:
+    ///   - data: The data to write to disk.
+    ///   - key: A unique key used to identify `data`.
     func syncCache(_ data: Data, key: String) throws
+
+    /// Synchronously get data from the cache. If data does not exist for `key`, an error will be thrown.
+    /// - Parameter key: A unique key used to identify `data`.
+    /// - Returns: An instance of Data which was previously stored on disk.
     func syncData(_ key: String) throws -> Data
+
+    /// Synchronously deletes cached data. If data does not exist for `key`, an error will be thrown.
+    /// - Parameter key: A unique key used to identify `data`.
     func syncDelete(_ key: String) throws
+
+    /// Deletes the cache directory and all or its contents, then recreates the cache directory.
     func syncDeleteAll() throws
+
+    /// Constructs the full file url for the given key. Useful for determiniing if a something is cached for the key.
+    /// - Parameter key: A unique key used to identify `data`.
+    /// - Returns: The file url for a cached it, based on `storageType`.
     func fileURL(_ key: String) -> URL
 
     // Async support
+
+    /// Asynchronously writes `data` to disk.
+    /// - Parameters:
+    ///   - data: The data to write to disk.
+    ///   - key: A unique key used to identify `data`.
     func cache(_ data: Data, key: String) async throws
+
+    /// Asynchronously get data from the cache. If data does not exist for `key`, an error will be thrown.
+    /// - Parameter key: A unique key used to identify `data`.
+    /// - Returns: An instance of Data which was previously stored on disk.
     func data(_ key: String) async throws -> Data
+
+    /// Asynchronously deletes cached data. If data does not exist for `key`, an error will be thrown.
+    /// - Parameter key: A unique key used to identify `data`.
     func delete(_ key: String) async throws
+
+    /// Deletes the cache directory and all or its contents, then recreates the cache directory.
     func deleteAll() async throws
 }
--- Sources/DiskCache/StorageType.swift
+++ Sources/DiskCache/StorageType.swift
@@ -7,14 +7,16 @@
 
 import Foundation
 
+/// An identifier specifying a group to which an app belongs.
 public typealias AppGroupID = String
 
+/// A location where data may be stored.
 public enum StorageType {
-    // stores data in user's `caches` directory, which is volatile
+    /// Stores data in user's `caches` directory, which is volatile.
     case temporary(_ subDirectory: SubDirectory? = nil)
-    // stores data in user's `directory` directory
+    /// Stores data in user's `directory` directory.
     case permanent(_ subDirectory: SubDirectory? = nil)
-    // stores data in shared container, which is suitable to share data between app, extenstions, etc
+    /// Stores data in shared container, which is suitable to share data between app, extenstions, etc.
     case shared(_ appGroupID: AppGroupID, subDirectory: SubDirectory? = nil)
 
     var subDirectory: String? {
